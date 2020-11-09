44fa122649cb7464234c581b519bf5809eef9b9c
Aleksandr Zhirkevich <sasha.zhirkevich@yandex.ru>
2020-11-09 18:26:54 +0300

    reworked manager

--- src/manage/Manage.java
+++ src/manage/Manage.java
@@ -1,65 +1,85 @@
 package manage;
 import client.Client;
-import message.Message;
-import message.MessageException;
-import protocol.command.Command;
-import protocol.command.CommandException;
-import protocol.result.Result;
-import protocol.result.ResultException;
+import com.sun.jdi.NativeMethodException;
+import protocol.Config;
 import server.Server;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.module.ResolutionException;
-import java.net.BindException;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Manage {
 
-	private static final String helpString = "Syntax:\n" + "run {server/client}\n";
+	private static final String helpString = "Syntax:\n" +
+			"run 'server' [port]\n" +
+			"    'client' [host] [port]}\n";
 
 	private static void error() {
 		System.err.println(helpString);
 		System.exit(1);
 	}
 
-	public static void main(String[] args) throws IOException, ConnectException {
-//		try {
-//			if (args.length != 2) {
-//				error();
-//			} else {
-//				if (args[0].equals("run")) {
-//					if (args[1].equals("server")) {
-//						Server s =new Server(8888);
-//						s.start();
-//						s.waitForTerminate();
-//					} else if (args[1].equals("client")) {
-//						new Client(new Socket("localhost", 8888)).start();
-//					} else
-//						error();
-//				} else
-//					error();
-//			}
-//		} catch (ConnectException e){
-//			System.err.println("Server is offline");
-//		}
-//		catch (FileNotFoundException e){
-//			System.err.println(e.getMessage());
-//		}
-//		catch (Exception e){
-//			e.printStackTrace(System.err);
-//		}
-//
-//		finally {
-//			new Scanner(System.in).next();
-//		}
-		try {
-			throw new ResultException(Result.INVALID);
+	public static void runServer(String[] args){
+		switch (args.length){
+			case 2:
+				new Server(Config.PORT).start(true);
+				break;
+			case 3:
+				try {
+					new Server(Integer.parseInt(args[2])).start(true);
+				} catch (NumberFormatException e) { error(); }
+				break;
+			default:
+				error();
 		}
-		catch (MessageException e){
-			e.printStackTrace();
+	}
+
+	public static void runClient(String[] args) throws IOException{
+		switch (args.length) {
+			case 2:
+				new Client(new Socket(Config.HOST, Config.PORT)).start();
+			case 4:
+				try {
+					new Client(new Socket(args[2], Integer.parseInt(args[3]))).start();
+				} catch (NativeMethodException e) { error(); }
+			default:
+				error();
+		}
+	}
+	public static void run(String[] args) throws IOException {
+		switch (args[1]){
+			case "server":
+				runServer(args);
+			case "client":
+				runClient(args);
+			default:
+				error();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			if (args.length <2 || args.length >4)
+				error();
+
+			switch (args[0]){
+				case "run":
+					run(args);
+			}
+
+		} catch (ConnectException e){
+			System.err.println("Server is offline");
+		}
+		catch (FileNotFoundException e){
+			System.err.println(e.getMessage());
+		}
+		catch (Exception e){
+			e.printStackTrace(System.err);
+		}
+		finally {
+			new Scanner(System.in).next();
 		}
 	}
 }
