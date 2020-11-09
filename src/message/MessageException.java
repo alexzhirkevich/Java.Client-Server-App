6538d018c14476e3076d7b56a17ed8f3d2421c07
Aleksandr Zhirkevich <sasha.zhirkevich@yandex.ru>
2020-11-09 20:38:09 +0300

    package reformat

--- src/message/MessageException.java
+++
@@ -1,8 +0,0 @@
-package message;
-
-public class MessageException extends Exception {
-
-	public MessageException(String str){
-		super(str);
-	}
-}
--- src/protocol/Config.java
+++
@@ -1,6 +0,0 @@
-package protocol;
-
-public interface Config {
-	int PORT = 8888;
-	String HOST = "localhost";
-}
